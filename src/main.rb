ead71650637c30f2a41ba0b8b1efb7aeb306f8ef
Miguel Martín <alu0101209777@ull.edu.es>
2021-02-26 00:31:23 +0000

    running

---
+++ src/main.rb
@@ -0,0 +1,11 @@
+require_relative '../require/vigenere-cipher'
+
+puts "Clave > "
+key = gets.chomp
+puts "Mensaje > "
+msg = gets.chomp
+
+vigenere = VigenereCipher.new(key)
+ciphered_msg = vigenere.cipher_message(msg)
+puts "Mensaje cifrado > " + vigenere.cipher_message(msg)
+puts "Mensaje descifrado > " + vigenere.decipher_message(ciphered_msg)
