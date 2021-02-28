0515eb421282cb7a5a1afec98ed68457f5aabc6d
Miguel Martín <alu0101209777@ull.edu.es>
2021-02-28 20:38:54 +0000

    copy button added

--- scripts/app.js
+++ scripts/app.js
@@ -37,7 +37,22 @@ function decipherMessage() {
   document.getElementById('message').value = message;
 }
 
+function copyCipheredToClipBoard() {
+  /* Get the text field */
+  var copyText = document.getElementById("cipheredMessage");
 
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Mensaje cifrado copiado");
+}
+
+window.copyCipheredToClipBoard =copyCipheredToClipBoard;
 window.updateKey = updateKey;
 window.decipherMessage = decipherMessage;
 window.cipherMessage = cipherMessage;
